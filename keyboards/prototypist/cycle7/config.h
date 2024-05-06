6edc0e4eefc79292f956fdcd9a250c2e80506eaf
Anjheos <Anjheos.Designs@gmail.com>
2024-05-06 02:39:20 -0700

    Initial commit for P[T] Cycle7 Hotswap ISO.

---
+++ keyboards/prototypist/cycle7/config.h
@@ -0,0 +1,9 @@
+// Copyright 2024 Anjheos (@Anjheos)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define WS2812_SPI SPID2
+#define WS2812_SPI_MOSI_PAL_MODE 5
+
+#define RGB_DI_PIN B15
